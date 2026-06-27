a80e4d76f819855289d7fa333680a53d42706df2
HotCheese <muhammedibrahim613@gmail.com>
2026-06-27 15:33:51 +0500

    Learned about button and pressable in react native and resolved sdk problem

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, TouchableOpacity, Pressable, Alert } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+
+const onButtonPress = () => Alert.alert("You pressed the button")
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider >
+      <SafeAreaView >
+        <Pressable onPress={onButtonPress}>
+
+          <Text>Touch the button</Text>
+        </Pressable>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
